1e16699037ffca7d5b9a089a21d6cb7657143799
Nikitha0706 <nikithasagarkommula@gmail.com>
2026-06-23 13:26:45 +0530

    feat:added javascript exercises

---
+++ Java Script/javascript.js
@@ -0,0 +1,120 @@
+//Functions
+
+// function myfun(a,b){
+//     console.log(a + b);
+// }
+// myfun(3,1)
+
+//function expression
+
+// const fun = function (a,b) {
+//     console.log(a-b);
+// }
+// fun(2,1)
+
+ //Arrow Function
+
+// const fun = (a,b) => {
+//     return a + b
+// }
+// console.log(fun(1,2))
+
+
+//const fun = (a,b) => a + b
+//console.log(fun(3 , 1))
+
+
+//Arrays
+// let data = [10,20.5,"hello",100,true]
+// console.log(data)
+
+// let data = [10,20.5,"hello",100,true]
+// console.log(data[2])
+
+
+// let data = [10,20.5,"hello",100,true]
+// for (let i=0 ; i < data.length; i++){
+//     console.log(data[i])
+// }
+
+// let data1= [10,344.098,"hello",true]
+// let data2= [100,300,200]
+// let newdata=[11,...data1, 30, ...data2]
+// console.log(newdata)
+
+//adds elements in the last
+// let data= [10,344.098,"hello",true]
+// data.push(100)
+// console.log(data)
+
+//deletes element in the last
+// let data= [10,344.098,"hello",true,100]
+// data.pop()
+// console.log(data)
+
+//adding elements in first
+// let data= [10,344.098,"hello",true,100]
+// data.unshift(200)
+// console.log(data)
+
+
+//Removes first element
+// let data= [10,344.098,"hello",true,100]
+// data.shift()
+// console.log(data)
+
+
+//objects
+
+//JSON FILES(JAVA SCRIPT OBJECT NOTATION)
+
+// let data = {
+//     id:101,
+//     name:"niki",
+//     age:20
+// }
+// console.log(data)
+
+// let data = [
+//     {
+//         id:101,
+//         name:"abc",
+//         age:16
+
+//     },
+//     {
+//         id:102,
+//         name:"cde",
+//         age:24
+//     },
+//     {
+//         id:103,
+//         name:"hbgj",
+//         age:10
+//     }
+
+// ]
+// console.log(data)
+
+
+// let data = [
+//      {
+//        id:101,
+//         name:"abc",
+//         age:16,
+//         techstack: ["python","Django","React"]
+
+//      },
+//      {
+//          id:102,
+//          name:"cde",
+//          age:24
+//      },
+//      {
+//          id:103,
+//          name:"hbgj",
+//          age:10
+//      }
+
+//  ]
+//  console.log(data[0].techstack[0]);
---
+++ Java Script/script.js
@@ -0,0 +1,39 @@
+/*console.log("hello")
+console.log(4 + 6)
+
+
+var num = 45;
+let num = 55;
+const num2 = 23;
+
+
+var a = 10;
+var a = 20;
+console.log(a);
+
+
+let a = 10;
+a = 20;
+console.log(a)
+
+const a = 10;
+console.log(a);*/
+
+//ES6 _ ECMA script 2015
+/*let a = 10
+let b = 20
+if (a > b) {
+    console.log("hiii");
+} else {
+    console.log("byee");
+}
+if (false) {
+
+} else if (false) {
+
+} else {
+
+}*/
+for (let i=1; i<=5; i++){
+    console.log(i)
+}
